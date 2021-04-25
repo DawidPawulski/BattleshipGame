354122a84111c9acdcd46e561c4e4c24632cf920
Dawid Pawulski <dpawulski@gmail.com>
2021-04-25 12:50:04 +0200

    Create RowNames enum

---
+++ Battleship/GameAPI/Models/Enums/RowNames.cs
@@ -0,0 +1,16 @@
+namespace GameAPI.Models.Enums
+{
+    public enum RowNames
+    {
+        ARow,
+        BRow,
+        CRow,
+        DRow,
+        ERow,
+        FRow,
+        GRow,
+        HRow,
+        IRow,
+        JRow
+    }
+}
