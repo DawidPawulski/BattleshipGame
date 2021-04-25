25acb2fd2b944947ef103ce056e85a7728799c67
Dawid Pawulski <dpawulski@gmail.com>
2021-04-25 18:30:23 +0200

    Add migration for creating new properties (HitsList and Orientation)

---
+++ Battleship/GameAPI/Migrations/20210425150146_AddOrientationForShipAndHitsListForBoard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameAPI.Migrations
+{
+    public partial class AddOrientationForShipAndHitsListForBoard : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NearHits",
+                table: "Boards",
+                newName: "ShipNearFields");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Orientation",
+                table: "Ships",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<List<int>>(
+                name: "HitsList",
+                table: "Boards",
+                type: "integer[]",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Orientation",
+                table: "Ships");
+
+            migrationBuilder.DropColumn(
+                name: "HitsList",
+                table: "Boards");
+
+            migrationBuilder.RenameColumn(
+                name: "ShipNearFields",
+                table: "Boards",
+                newName: "NearHits");
+        }
+    }
+}
