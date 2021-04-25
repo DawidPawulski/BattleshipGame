b7d4b8e0bbd21354d6c01a6c233e47400c061614
Dawid Pawulski <dpawulski@gmail.com>
2021-04-25 12:49:48 +0200

    Create MoveMessages enum

---
+++ Battleship/GameAPI/Models/Enums/MoveMessages.cs
@@ -0,0 +1,16 @@
+namespace GameAPI.Models.Enums
+{
+    public enum MoveMessages
+    {
+        Miss,
+        Hit,
+        BattleshipDestroyed,
+        CarrierDestroyed,
+        CruiserDestroyed,
+        DestroyerDestroyed,
+        SubmarineDestroyed,
+        PatrolBoatDestroyed,
+        TacticalBoatDestroyed,
+        Win
+    }
+}
