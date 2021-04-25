62bb3723d4394a0d4dac1205922403c4d6c99e7e
Dawid Pawulski <dpawulski@gmail.com>
2021-04-25 21:54:37 +0200

    Create FieldValues enum in react

---
+++ Battleship/battleship-interface/src/enums/FieldValuesEnum.ts
@@ -0,0 +1,18 @@
+export enum FieldValues {
+    Empty,
+    Miss,
+    Battleship,
+    Carrier,
+    Cruiser,
+    Destroyer,
+    Submarine,
+    PatrolBoat,
+    TacticalBoat,
+    NearBattleship,
+    NearCarrier,
+    NearCruiser,
+    NearDestroyer,
+    NearSubmarine,
+    NearPatrolBoat,
+    NearTacticalBoat
+}
