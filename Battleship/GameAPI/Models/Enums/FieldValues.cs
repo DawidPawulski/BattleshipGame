e9ba2c7ccc74aff3a36a227cb7970ba6addae6fc
Dawid Pawulski <dpawulski@gmail.com>
2021-04-25 12:49:09 +0200

    Create FieldValues enum

---
+++ Battleship/GameAPI/Models/Enums/FieldValues.cs
@@ -0,0 +1,22 @@
+namespace GameAPI.Models.Enums
+{
+    public enum FieldValues
+    {
+        Empty,
+        Miss,
+        Battleship,
+        Carrier,
+        Cruiser,
+        Destroyer,
+        Submarine,
+        PatrolBoat,
+        TacticalBoat,
+        NearBattleship,
+        NearCarrier,
+        NearCruiser,
+        NearDestroyer,
+        NearSubmarine,
+        NearPatrolBoat,
+        NearTacticalBoat
+    }
+}
