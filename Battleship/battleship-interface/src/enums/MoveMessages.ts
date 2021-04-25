9ef7f1a05e34ad02cd46c07516e7a5bf630b9715
Dawid Pawulski <dpawulski@gmail.com>
2021-04-25 21:55:03 +0200

    Create MoveMessages enum in react

---
+++ Battleship/battleship-interface/src/enums/MoveMessages.ts
@@ -0,0 +1,12 @@
+export enum MoveMessages {
+    Miss,
+    Hit,
+    BattleshipDestroyed,
+    CarrierDestroyed,
+    CruiserDestroyed,
+    DestroyerDestroyed,
+    SubmarineDestroyed,
+    PatrolBoatDestroyed,
+    TacticalBoatDestroyed,
+    Win
+}
