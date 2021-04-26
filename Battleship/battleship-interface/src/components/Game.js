20eaafc5a9c2727af90497fcd23080318a1a7b84
Dawid Pawulski <dpawulski@gmail.com>
2021-04-26 23:31:38 +0200

    Add handler for getting players to Game.js

--- Battleship/battleship-interface/src/components/Game.js
+++ Battleship/battleship-interface/src/components/Game.js
@@ -5,10 +5,20 @@ export class Game extends Component{
     constructor(props){
         super(props);
         this.state={
+            firstPlayer: {},
+            secondPlayer: {},
         };
     }
 
+    async handleGetPlayers() {
+        const pathToGetFirstPlayer = 'player/'+localStorage.getItem('firstPlayerId');
+        const pathToGetSecondPlayer = 'player/'+localStorage.getItem('secondPlayerId');
 
+        let firstPlayerResponse = await get(pathToGetFirstPlayer);
+        let secondPlayerResponse = await get(pathToGetSecondPlayer);
+        this.setState({firstPlayer: firstPlayerResponse});
+        this.setState({secondPlayer: secondPlayerResponse});
+    }
 
     render(){
     }
