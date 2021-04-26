8020cf149ce53729ab134ce0f755a58d6fa013a7
Dawid Pawulski <dpawulski@gmail.com>
2021-04-26 23:15:28 +0200

    Update DB migrations after changing property name (IsHit)

---
+++ Battleship/GameAPI/Migrations/20210426210702_ChangeNameOfFieldProperty.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameAPI.Migrations
+{
+    public partial class ChangeNameOfFieldProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "isHit",
+                table: "Fields",
+                newName: "IsHit");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsHit",
+                table: "Fields",
+                newName: "isHit");
+        }
+    }
+}
