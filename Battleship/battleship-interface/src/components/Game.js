2b1d50e9c795181fec99923d7491858bdfd7d57a
Dawid Pawulski <dpawulski@gmail.com>
2021-04-26 23:30:16 +0200

    Create game component

---
+++ Battleship/battleship-interface/src/components/Game.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+export class Game extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+        };
+    }
+
+
+
+    render(){
+    }
+}
