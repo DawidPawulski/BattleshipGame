c4ba4ea9be9aec997fa72b5d05243090740395b8
Dawid Pawulski <dpawulski@gmail.com>
2021-04-27 19:53:21 +0200

    Add unit test when creating board

--- Battleship/GameAPITests/ModelsTests/BoardTests.cs
+++ Battleship/GameAPITests/ModelsTests/BoardTests.cs
@@ -1,10 +1,51 @@
+using System.Linq;
+using GameAPI.Models;
+using GameAPI.Models.Enums;
 using NUnit.Framework;
 
-namespace GameAPITests
+namespace GameAPITests.ModelsTests
 {
     [TestFixture]
     public class BoardTests
     {
-        
+        private Board _board;
+
+        [SetUp]
+        public void Setup()
+        {
+            _board = new Board();
+            _board.CreateBoard();
+        }
+
+        [Test]
+        public void Board_WhenCallingCreateBoardMethod_ShouldCreateListWithFields()
+        {
+            const int numberOfFieldsAssignedToBoard = 100;
+            int counter = 0;
+            
+            _board.Fields.ForEach(x => counter++);
+            
+            Assert.AreEqual(numberOfFieldsAssignedToBoard, counter);
+        }
+
+        [Test]
+        public void Board_WhenCreatingNewBoard_FieldsFrom1To10ShouldBeInFirstRow()
+        {
+            var isEveryFieldInFirstRow = true;
+            var nameForFirstRow = RowNames.ARow;
+            var orderNumbersForFirstRow = Enumerable.Range(1, 10).ToArray();
+
+            foreach (var field in _board.Fields)
+            {
+                if (orderNumbersForFirstRow.Contains(field.OrderNumber))
+                {
+                    if (field.RowName != nameForFirstRow)
+                    {
+                        isEveryFieldInFirstRow = false;
+                    }
+                }
+            }
+            Assert.True(isEveryFieldInFirstRow);
+        }
     }
 }
