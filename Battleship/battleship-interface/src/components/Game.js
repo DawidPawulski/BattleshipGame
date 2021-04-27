a8cf14853d8f0a20fdec2eee8c736aed955ea710
Dawid Pawulski <dpawulski@gmail.com>
2021-04-27 20:50:43 +0200

    Add render content in Game.js

--- Battleship/battleship-interface/src/components/Game.js
+++ Battleship/battleship-interface/src/components/Game.js
@@ -87,5 +87,55 @@ export class Game extends Component{
     }
 
     render(){
+        let nameOfTheWinner = this.state.opponentPlayerId === this.state.firstPlayer.Id 
+                            ? this.state.secondPlayer.Name : this.state.firstPlayer.Name;
+
+        return (
+            <Container className="mt-5">
+                <Row>
+                    <Col className="text-center"><h2>Battleship Game</h2></Col>
+                </Row>
+                <Row className="mt-5 text-center">
+                    <Col className="mt-2">
+                        <Board label="First player name" spanId="firstPlayerName" playerName={this.state.firstPlayer.Name}
+                            boardDivId="firstPlayerBoard" boardColumns={this.state.boardColumns} boardRows={this.state.boardRows}
+                            playerFields={this.state.firstPlayerBoard.Fields}/>
+                    </Col>
+                    <Col>
+                        <div>
+                        <ButtonToolbar className="mt-5 d-flex justify-content-center">
+                            {!this.state.playGame ? 
+                                (<Button variant='success' onClick={()=>{
+                                    this.setState({playGame:true});
+                                    this.loop();
+                                }}>
+                                    Start game!
+                                </Button>)
+                                :
+                                    (<Button variant='danger' onClick={()=>{
+                                        this.setState({playGame: false}); 
+                                        clearInterval(this.state.intervalId);
+                                        }}>
+                                    Pause game!
+                                </Button>)
+                            }
+                        </ButtonToolbar>
+                        </div>
+                        <div className="war-log mt-3">
+                            {this.state.warLog.map((log, index) => {
+                                return (<div className="mt-2"><b>{log}</b><br/></div>)
+                            })}
+                        </div>
+                    </Col>
+                    <Col className="mt-2">
+                        <Board label="Second player name" spanId="secondPlayerName" playerName={this.state.secondPlayer.Name}
+                                boardDivId="secondPlayerBoard" boardColumns={this.state.boardColumns} boardRows={this.state.boardRows}
+                                playerFields={this.state.secondPlayerBoard.Fields}/>
+                    </Col>
+                </Row>
+                <EndGameModal show={this.state.endGameModalShow}
+                    playername={nameOfTheWinner}></EndGameModal>
+            </Container>
+      )
     }
 }
