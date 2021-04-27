eac6e835d86a15f147e57d3d89e6fa7d51585c50
Dawid Pawulski <dpawulski@gmail.com>
2021-04-27 20:20:37 +0200

    Add unit tests for ShipPlaceHelper

--- Battleship/GameAPITests/HelpersTests/ShipPlaceHelperTests.cs
+++ Battleship/GameAPITests/HelpersTests/ShipPlaceHelperTests.cs
@@ -1,14 +1,69 @@
+using System.Linq;
 using GameAPI.Helpers;
 using GameAPI.Models;
 using GameAPI.Models.Enums;
-using Microsoft.EntityFrameworkCore.Infrastructure;
 using NUnit.Framework;
 
-namespace GameAPITests
+namespace GameAPITests.HelpersTests
 {
     [TestFixture]
     public class ShipPlaceHelperTests
     {
+        private Player _player;
+        private Board _board;
+        
+        [SetUp]
+        public void Setup()
+        {
+            _player = new Player();
+            _board = new Board();
+            _board.CreateBoard();
+            _player.Board = _board;
+        }
 
+        [Test]
+        public void ShipPlaceHelper_WhenPlacingShipsOnBoard_FieldsShouldChangeValuesForAllShips()
+        {
+            const int numberOfFieldsTakenByShips = 18;
+            var fieldValuesForShips = new[]
+            {
+                FieldValues.Battleship, FieldValues.Carrier, FieldValues.Cruiser, FieldValues.Destroyer,
+                FieldValues.Submarine, FieldValues.PatrolBoat, FieldValues.TacticalBoat
+            };
+            
+            ShipPlaceHelper.PlaceShips(_player);
+
+            var counter = _board.Fields.Count(field => fieldValuesForShips.Contains(field.Value));
+
+            Assert.AreEqual(numberOfFieldsTakenByShips, counter);
+        }
+        
+        [Test]
+        public void ShipPlaceHelper_WhenPlacingShipsOnBoard_FourFieldsShouldHaveValueBattleship()
+        {
+            const int numberOfFieldsTakenByBattleship = 4;
+            const FieldValues fieldValueForBattleship = FieldValues.Battleship;
+
+            ShipPlaceHelper.PlaceShips(_player);
+
+            var counter = _board.Fields.Count(field => field.Value == fieldValueForBattleship);
+
+            Assert.AreEqual(numberOfFieldsTakenByBattleship, counter);
+        }
+        
+        [Test]
+        public void ShipPlaceHelper_WhenPlacingShipsOnBoard_AllIsHitPropertiesShouldBeSetToFalse()
+        {
+            var allIsHitPropertiesAreSetToFalse = true;
+            
+            ShipPlaceHelper.PlaceShips(_player);
+
+            foreach (var field in _board.Fields.Where(field => field.IsHit))
+            {
+                allIsHitPropertiesAreSetToFalse = false;
+            }
+            
+            Assert.True(allIsHitPropertiesAreSetToFalse);
+        }
     }
 }
