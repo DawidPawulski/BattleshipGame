d905f032d4f48af842d1d3abfe2f3dc368e5f88d
Dawid Pawulski <dpawulski@gmail.com>
2021-04-27 19:29:42 +0200

    New Player tests: check name assignment and ship list

--- Battleship/GameAPITests/ModelsTests/PlayerTests.cs
+++ Battleship/GameAPITests/ModelsTests/PlayerTests.cs
@@ -1,10 +1,37 @@
+using GameAPI.Models;
 using NUnit.Framework;
 
-namespace GameAPITests
+namespace GameAPITests.ModelsTests
 {
     [TestFixture]
     public class PlayerTests
     {
+        private Player _player;
+        private Board _board;
         
+        [SetUp]
+        public void Setup()
+        {
+            _player = new Player {Name = "Dawid"};
+            _board = new Board();
+            _board.CreateBoard();
+            _player.Board = _board;
+        }
+
+        [Test]
+        public void Player_WhenCreateNewPlayer_ShouldCreateNewPlayerShips()
+        {
+            const int numberOfCreatedShips = 7;
+            
+            Assert.AreEqual(numberOfCreatedShips, _player.Ships.Count);
+        }
+
+        [Test]
+        public void Player_WhenCreateNewPlayerWithName_NamePropertyShouldReturnPlayerName()
+        {
+            var assignedPlayerName = "Dawid";
+            
+            Assert.AreEqual(assignedPlayerName, _player.Name);
+        }
     }
 }
