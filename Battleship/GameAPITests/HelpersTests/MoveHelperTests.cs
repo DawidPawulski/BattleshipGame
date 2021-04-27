8a69d14cd8f6746bb40d4406d277a91b844f4ac0
Dawid Pawulski <dpawulski@gmail.com>
2021-04-27 20:38:50 +0200

    Create MoveHelper unit tests

--- Battleship/GameAPITests/HelpersTests/MoveHelperTests.cs
+++ Battleship/GameAPITests/HelpersTests/MoveHelperTests.cs
@@ -1,26 +1,56 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
 using GameAPI.Helpers;
 using GameAPI.Models;
+using GameAPI.Models.Enums;
 using NUnit.Framework;
 
-namespace GameAPITests
+namespace GameAPITests.HelpersTests
 {
     [TestFixture]
     public class MoveHelperTests
     {
+        private Player _player;
+        private Board _board;
+        
         [SetUp]
         public void Setup()
         {
-
+            _player = new Player();
+            _board = new Board();
+            _board.CreateBoard();
+            _player.Board = _board;
         }
 
         [Test]
-        public void Should()
+        public void MakeMove_WhenFieldListIsEmpty_ShouldThrowArgumentOutOfRangeException()
         {
-            //Arrange
+            _board.Fields = new List<Field>();
             
-            //Act
+            Assert.Throws<ArgumentOutOfRangeException>(() => MoveHelper.MakeMove(_player, _board));
+        }
+        
+        [Test]
+        public void MakeMove_WhenPlayerMakesFirstMove_OnlyOneFieldChangesIsHitToTrue()
+        {
+            const int numberOfFieldsChanged = 1;
             
-            //Assert
+            MoveHelper.MakeMove(_player, _board);
+
+            var counter = _board.Fields.Count(x => x.IsHit);
+            
+            Assert.AreEqual(numberOfFieldsChanged, counter);
+        }
+        
+        [Test]
+        public void MakeMove_WhenPlayerMakesFirstMove_ReturnMoveMessageIsOnlyHitOrMiss()
+        {
+            var availableReturnMessages = new MoveMessages[] {MoveMessages.Hit, MoveMessages.Miss};
+            
+            MoveHelper.MakeMove(_player, _board);
+            
+            Assert.IsTrue(availableReturnMessages.Contains(_board.Message));
         }
     }
 }
